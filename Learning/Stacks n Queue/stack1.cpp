788d3f5b722736e1df18d01cd0a77e0aa879f735
Bindusree1515 <91887086+Bindusree1515@users.noreply.github.com>
2024-02-12 17:42:08 +0530

    Create stack1.cpp

---
+++ Learning/Stacks n Queue/stack1.cpp
@@ -0,0 +1,21 @@
+// l38
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  //declaration
+    stack <int> s ; 
+  
+  //insertion
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+
+  //accessing
+    while(!s.empty()){
+        cout << s.top() << endl;
+        s.pop();
+    }
+
+return 0;
+}
